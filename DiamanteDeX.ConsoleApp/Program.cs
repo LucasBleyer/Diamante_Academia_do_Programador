4977dc5a2ff3c22cd360bc1152f7fb0003235295
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 17:04:26 -0300

    quadrado de x

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -6,7 +6,23 @@ namespace DiamanteDeX.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            string metade_diamante_cima_pra_baixo = "";
+            int numero;
+            do
+            {
+                Console.Write("Digite um número ímpar: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            } while (numero % 2 == 0);
+
+            for (int i = 0; i < numero / 2; i++)
+            {
+                for (int j = 0; j < numero / 2; j++)
+                {
+                    metade_diamante_cima_pra_baixo += "x";
+                }
+                metade_diamante_cima_pra_baixo += "\n";
+            }
+
         }
     }
 }
