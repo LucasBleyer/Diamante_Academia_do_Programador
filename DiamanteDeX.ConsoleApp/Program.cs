472e81ac66b685ee807f3c2a67d7c262d27f9b3b
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 17:52:42 -0300

    triangulo

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -6,22 +6,32 @@ namespace DiamanteDeX.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string metade_diamante_cima_pra_baixo = "";
-            int numero;
+            int numero, espaco, x = 1;
+
             do
             {
-                Console.Write("Digite um número ímpar: ");
+                Console.Write("Digite um número inteiro e ímpar: ");
                 numero = Convert.ToInt32(Console.ReadLine());
             } while (numero % 2 == 0);
 
-            for (int i = 0; i < numero / 2; i++)
+            espaco = (numero - 1) / 2;
+
+            for (int i = espaco; i > 0; i--)
             {
-                for (int j = 0; j < numero / 2; j++)
+                for (int j = 1; j <= i; j++)
                 {
-                    metade_diamante_cima_pra_baixo += "x";
+                    Console.Write(" ");
                 }
-                metade_diamante_cima_pra_baixo += "\n";
+                for (int k = 1; k <= x; k++)
+                {
+                    Console.Write("x");
+                }
+                x += 2;
+                Console.WriteLine();
             }
+            
+
+
 
         }
     }
