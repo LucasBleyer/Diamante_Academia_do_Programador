e81193b0b4588222e299c325f2235d486b5d95b7
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 22:28:54 -0300

    metade

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -6,7 +6,29 @@ namespace DiamanteDeX.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+        int numero, espaco, x = 1;
+
+            do
+            {
+                Console.Write("Digite um número inteiro e ímpar: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            } while (numero % 2 == 0);
+
+            espaco = (numero - 1) / 2;
+
+            for (int i = espaco; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= x; k++)
+                {
+                    Console.Write("x");
+                }
+                x += 2;
+                Console.WriteLine();
+            }
         }
     }
 }
