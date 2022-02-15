f5e794b872be64de5f4022be8f80e5e67c2102dd
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-14 21:38:08 -0300

    finalizado

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -6,33 +6,63 @@ namespace DiamanteDeX.ConsoleApp
     {
         static void Main(string[] args)
         {
-            int numero, espaco, x = 1;
+            string x = "x", espaco = " ";
+            int numero;
 
             do
             {
-                Console.Write("Digite um número inteiro e ímpar: ");
+                Console.Write("Digite um número inteiro e ímpar:");
                 numero = Convert.ToInt32(Console.ReadLine());
             } while (numero % 2 == 0);
-
-            espaco = (numero - 1) / 2;
-
-            for (int i = espaco; i > 0; i--)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int k = 1; k <= x; k++)
-                {
-                    Console.Write("x");
-                }
-                x += 2;
-                Console.WriteLine();
-            }
             
+            int metade = (numero + 1) / 2;
 
+            for (int i = 1; i <= numero; i++)
+            {
+                if (i == 1)
+                {
+                    for (int j = 1; j <= (metade - i); j++)
+                    {
+                        Console.Write(espaco);
+                    }
+                    Console.WriteLine(x);
+                }
+                else if (i <= metade)
+                {
+                    x = x + "xx";
+                    for (int j = 1; j <= (metade - i); j++)
+                    {
+                        Console.Write(espaco);
+                    }
+                    Console.WriteLine(x);
+                }
+            }
 
-
+            for (int i = 1; i <= numero; i++)
+            {
+                x = "x";
+                if (i == numero)
+                {
+                    for (int m = metade; m < numero; m++)
+                    {
+                        Console.Write(espaco);
+                    }
+                    Console.WriteLine(x);
+                }
+                else if (i > metade)
+                {
+                    x = x + "xx";
+                    for (int k = i - metade; k > 0; k--)
+                    {
+                        Console.Write(espaco);
+                    }
+                    for (int j = 2; j <= (numero - i); j++)
+                    {
+                        x = x + "xx";
+                    }
+                    Console.WriteLine(x);
+                }
+            }
         }
     }
 }
